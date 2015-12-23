0d60da189ef2d02632ddefafddc0dbb5db96abf4
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-12-23 12:08:35 +0000

    Re #13738 Get run label for non-consecutive groups, add tests
    E.g. "MUSR00015193-5, 15196-7, 15199"

--- MantidQt/CustomInterfaces/test/MuonAnalysisHelperTest.h
+++ MantidQt/CustomInterfaces/test/MuonAnalysisHelperTest.h
@@ -60,18 +60,36 @@ public:
     TS_ASSERT_EQUALS(label, "MUSR00015189-93");
   }
 
-  void test_getRunLabel_wsList_wrongOrder()
-  {
-    std::vector<int> runNumbers = boost::assign::list_of(10)(3)(5)(1)(6);
+  void test_getRunLabel_wsList_wrongOrder() {
+    std::vector<int> runNumbers{10, 3, 5, 1, 6, 2, 4, 8, 7, 9};
     std::vector<Workspace_sptr> list;
 
-    for (auto it = runNumbers.begin(); it != runNumbers.end(); ++it)
-    {
+    for (auto it = runNumbers.begin(); it != runNumbers.end(); ++it) {
       list.push_back(createWs("EMU", *it));
     }
 
     std::string label = getRunLabel(list);
     TS_ASSERT_EQUALS(label, "EMU00000001-10");
+  }
+
+  void test_getRunLabel_wsList_nonConsecutive() {
+    std::vector<int> runNumbers{1, 2, 3, 5, 6, 8, 10, 11, 12, 13, 14};
+    std::vector<Workspace_sptr> list;
+    for (auto it = runNumbers.begin(); it != runNumbers.end(); it++) {
+      list.push_back(createWs("EMU", *it));
+    }
+    std::string label = getRunLabel(list);
+    TS_ASSERT_EQUALS(label, "EMU00000001-3, 5-6, 8, 10-4");
+  }
+
+  void test_getRunLabel_wsList_nonConsecutive_wrongOrder() {
+    std::vector<int> runNumbers{5, 14, 8, 1, 11, 3, 10, 6, 13, 12, 2};
+    std::vector<Workspace_sptr> list;
+    for (auto it = runNumbers.begin(); it != runNumbers.end(); it++) {
+      list.push_back(createWs("EMU", *it));
+    }
+    std::string label = getRunLabel(list);
+    TS_ASSERT_EQUALS(label, "EMU00000001-3, 5-6, 8, 10-4");
   }
 
   void test_sumWorkspaces()
